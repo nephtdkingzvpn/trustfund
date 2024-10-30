c4da7b9be9b98d57f268a666aac6899084d8490a
NephygTdkingz <tdkingzict@gmail.com>
2024-10-30 11:55:50 +0100

    prepared for first deployment

--- trustfund_bnk/settings.py
+++ trustfund_bnk/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost','trustfundonline.top', 'www.trustfundonline.top']
 
 
 # Application definition
