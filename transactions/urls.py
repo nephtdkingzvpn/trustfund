92c7e20d85ebc16ecfd47c5ab8947872b92a4af4
NephygTdkingz <tdkingzict@gmail.com>
2024-10-30 09:57:52 +0100

    initial commit

---
+++ transactions/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'transactions'
+
+urlpatterns = [
+    path("deposit/", views.DepositMoneyView.as_view(), name="deposit_money"),
+    path("withdraw/", views.WithdrawMoneyView.as_view(), name="withdraw_money"),
+    path("account/all_transactions/", views.TransactionListView.as_view(), name="all_transactions"),
+    path("delete_transaction/<pk>/", views.delete_transaction, name="delete_transaction"),
+    path("account/transfer/", views.CustomerWithdrawMoneyView.as_view(), name="customer_transfer"),
+    path("transaction_successful/", views.transaction_successful, name="transaction_successful"),
+    path("transaction_failed/", views.transaction_failed, name="transaction_failed"),
+]
